c6a8cb6471f566ef0ea5f50a61dec397aafe317e
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-11 16:11:29 -0300

    feat: using the route initializer in the router.go file

--- router/router.go
+++ router/router.go
@@ -5,12 +5,10 @@ import "github.com/gin-gonic/gin"
 func Initialize() {
 	// Initialize the router with default Gin settings
 	router := gin.Default()
-	// Defining an endpoint
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	// Running api on port 8080 
+
+	// Initialize routes
+	initializeRoutes(router)
+
+	// Running the server
 	router.Run(":8080")
 }
