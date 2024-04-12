9f148aa2d85abb780f84074da1e1e8231f397717
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-12 16:01:55 -0300

    feat: using the message generated by the logger for future database connection

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/reishenrique/job-gopportunities/router"
+import (
+	"github.com/reishenrique/job-gopportunities/config"
+	"github.com/reishenrique/job-gopportunities/router"
+) 
 
-func main() {
+var (
+	logger *config.Logger
+)
+
+func main() {	
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.InitDatabase()
+	if err != nil {
+		logger.Errorf("Initialization Database Error: %v", err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
