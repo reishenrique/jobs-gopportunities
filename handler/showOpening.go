6f1edf6d8610cd3762db1a43acb30abe1d244a5c
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-16 10:36:55 -0300

    feat: find and show opening with id provided

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,28 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/reishenrique/job-gopportunities/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H {
-		"message": "GET Opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id",
+		"queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("Opening with id: %v not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
