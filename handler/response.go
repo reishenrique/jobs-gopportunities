6f30004f72ba1f101ef581de8548abeb13eeb7b6
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-16 15:43:04 -0300

    feat: creating documentation for the 'create opening' endpoint with swagger

--- handler/response.go
+++ handler/response.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/reishenrique/job-gopportunities/schemas"
 )
 
 func sendError(ctx *gin.Context, code int, message string) {
@@ -23,5 +24,14 @@ func sendSuccess(ctx *gin.Context, operation string, data interface{}) {
 		"message": fmt.Sprintf("operation from handler: %s successfull", operation),
 		"data": data,
 	})
+}
 
+type ErrorResponse struct {
+	Message string `json:"message"`
+	ErrorCode string `json:"data"`
+}
+
+type CreateOpeningResponse struct {
+	Message string `json:"message"`
+	Data schemas.OpeningResponse `json:"data"`
 }
