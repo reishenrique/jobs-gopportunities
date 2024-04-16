7d06459ec3a2c2f02e7de81883758e62f9fa60a8
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-16 15:27:55 -0300

    feat: add swagger configuration and running scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+- Make script para rodar o executável da aplicação juntamente com a documentação:
+    `make`
+    
--- router/routes.go
+++ router/routes.go
@@ -3,13 +3,21 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/reishenrique/job-gopportunities/handler"
+
+	docs "github.com/reishenrique/job-gopportunities/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize handler
 	handler.InitializeHandler()
+
+	basePath := "/api/v1"
+
+	docs.SwaggerInfo.BasePath = basePath
 	
-	v1 := router.Group("/api/v1/")
+	v1 := router.Group("basePath")
 
 	{
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -22,4 +30,7 @@ func initializeRoutes(router *gin.Engine) {
 
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
