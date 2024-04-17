75e91dcc3067fab0093d2b7808f7a10f549e174f
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-17 15:28:19 -0300

    feat: create update opening swagger documentation

--- handler/response.go
+++ handler/response.go
@@ -50,3 +50,8 @@ type ShowOpeningResponse struct {
 	Message string                  `json:"message"`
 	Data    schemas.OpeningResponse `json:"data"`
 }
+
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
