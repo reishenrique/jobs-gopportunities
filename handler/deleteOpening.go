ab6fd77d08e7fa1245b95bb5a9a709a28685c579
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-17 15:23:14 -0300

    feat: delete opening swagger documentation

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -8,6 +8,18 @@ import (
 	"github.com/reishenrique/job-gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Delete opening
+// @Description Delete a job opening by id
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} DeleteOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [delete]
 func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 
--- handler/response.go
+++ handler/response.go
@@ -35,3 +35,8 @@ type CreateOpeningResponse struct {
 	Message string `json:"message"`
 	Data schemas.OpeningResponse `json:"data"`
 }
+
+type DeleteOpeningResponse struct {
+	Message string `json:"message"`
+	Data schemas.OpeningResponse `json:"data"`
+}
