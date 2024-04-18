c4a67426bd4a354252a314d105ae894b5bd6cdbb
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-18 16:13:59 -0300

    feat: handler responsible for creating a recruiter

---
+++ handler/recruiter/createRecruiter.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/reishenrique/job-gopportunities/schemas"
+)
+
+func CreateRecruiterHandler(ctx *gin.Context) {
+	request := CreateRecruiterRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation Error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	recruiter := schemas.Recruiters {
+		FullName: request.FullName,
+		Recruiter: *request.Recruiter,
+		CompanyName: request.CompanyName,
+		CompanyEmail: request.CompanyEmail,
+		CompanyWebsite: request.CompanyWebsite,
+		Phone: request.Phone,
+	}
+
+	if err := db.Create(&recruiter).Error; err != nil {
+		logger.Errorf("Error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error saving recruiter to the database")
+		return
+	}
+
+	sendSuccess(ctx, "create-recruiter", recruiter)
+}
