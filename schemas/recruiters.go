0dc5e24549e73c58ba87c6e9b987d4e8670bcc8c
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-18 16:15:04 -0300

    feat: schema of recruiters and response data

---
+++ schemas/recruiters.go
@@ -0,0 +1,30 @@
+package schemas
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Recruiters struct {
+	gorm.Model
+	FullName string
+	Recruiter bool
+	CompanyName string
+	CompanyEmail string
+	CompanyWebsite string
+	Phone int64
+}
+
+type RecruitersResponse struct {
+	ID uint `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"UpdatedAt"`
+	DeletedAt time.Time `json:"DeletedAt"`
+	FullName string `json:"fullName"`
+	Recruiter bool `json:"recruiter"`
+	CompanyName string `json:"companyName"`
+	CompanyEmail string `json:"companyEmail"`
+	CompanyWebsite string `json:"companyWebsite"`
+	Phone int64 `json:"phone"`
+}
